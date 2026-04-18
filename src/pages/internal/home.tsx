bd0991bef93f1ab03ceb873525e63caec7594031
Redgale <150551630+Redgale@users.noreply.github.com>
2026-04-18 14:43:23 -0400

    Update home.tsx

--- src/pages/internal/home.tsx
+++ src/pages/internal/home.tsx
@@ -83,9 +83,9 @@ function InternalHome() {
             <div className="footer">
                 <Obfuscated>RedGale inc. 2026 | </Obfuscated>
                 <a target="_blank" href={discord}>
-                    <Obfuscated>Discord</Obfuscated>
+                    <Obfuscated></Obfuscated>
                 </a>
-                <Obfuscated> / </Obfuscated>
+                <Obfuscated></Obfuscated>
                 <a target="_blank" href={github}>
                     <Obfuscated>Source</Obfuscated>
                 </a>
